ef9da341babea59941eb52cf79321ca73e156781
Temesgen Abdissa <temesgenabdissa2@gmail.com>
2024-01-11 13:02:59 +0000

    add.py

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Write a type-annotated function add
+that takes a float a and a float b 
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    function add that takes a float
+    a and a float b
+    """
+    return a + b
